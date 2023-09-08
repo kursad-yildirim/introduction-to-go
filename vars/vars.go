eb250b0e78f9d2feddb912fe27e16060b4176692
kursad-yildirim <kursad.yildirim@outlook.com>
2023-09-08 12:11:43 +0200

    vars 1

--- vars/vars.go
+++ vars/vars.go
@@ -4,6 +4,12 @@ import "fmt"
 
 var x, y int = 4, 5
 
+type cardface string
+
+type deck struct {
+	faces []cardface
+}
+
 func main() {
 	var i, j int
 	i = 1
@@ -14,11 +20,20 @@ func main() {
 	fmt.Printf("%v is %v, which is %v\n", num1, str1, istrue)
 	fmt.Printf("%#v is %#v, which is %#v\n", num1, str1, istrue)
 
-	faces := []string{"spades", "clubs", "hearts"}
+	var diamondface cardface = "diamonds"
+	faces := []cardface{"spades", "clubs", "hearts"}
 	fmt.Println("Missing one face:", faces, len(faces), cap(faces))
-	faces = append(faces, "diamonds")
+	faces = append(faces, diamondface)
 	fmt.Println("Appended face", faces, len(faces), cap(faces))
 
 	blackfaces := faces[0:2]
 	fmt.Println("Black faces", blackfaces, len(blackfaces), cap(blackfaces))
+	var mydeck deck
+	for _, face := range faces {
+		mydeck.faces = append(mydeck.faces, face)
+		fmt.Println(face, " is added to  the deck as a new face")
+	}
+	fmt.Println(mydeck)
+	fmt.Printf("my deck is %v\n", mydeck)
+	fmt.Printf("my deck is %#v\n", mydeck)
 }
