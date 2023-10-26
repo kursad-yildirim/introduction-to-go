569b277a203f093f10e2ffd314ee19d35f040119
kursad-yildirim <kursad.yildirim@outlook.com>
2023-10-26 17:49:11 +0200

    interfaces basics

--- 3.interfaces/interfaces.go
+++ 3.interfaces/interfaces.go
@@ -3,78 +3,43 @@ package main
 import "fmt"
 
 type triangle struct {
-	height int
-	base   int
+	height float64
+	base   float64
 }
 
 type rectangle struct {
-	side1 int
-	side2 int
+	side1 float64
+	side2 float64
 }
 
 type square struct {
-	side int
+	side float64
 }
 
 type shape interface {
-	pArea()
+	calcArea() float64
 }
 
 func main() {
-	t := triangle{height: 2, base: 6}
-	r := rectangle{4, 4}
+	t := triangle{4, 7}
 	s := square{5}
-	sh := []shape{t, r, s}
-
-	fmt.Println(t, r, s)
-
-	sh[0].pArea()
-	sh[1].pArea()
-	sh[2].pArea()
+	r := rectangle{6, 9}
 
 	printArea(t)
-	printArea(r)
 	printArea(s)
-}
-
-/*
-func triangleArea(t triangle) float32 {
-	return 0.5 * float32(t.base) * float32(t.height)
-}
-
-func rectangleArea(r rectangle) int {
-	return r.side1 * r.side2
-}
-
-func squareArea(s square) int {
-	return s.side * s.side
-}
-*/
-
-func (t triangle) areaIs() float32 {
-	return 0.5 * float32(t.base) * float32(t.height)
-}
-
-func (r rectangle) areaIs() int {
-	return r.side1 * r.side2
-}
-
-func (s square) areaIs() int {
-	return s.side * s.side
-}
-
-func (t triangle) pArea() {
-	fmt.Println("Triangle: ", t.areaIs())
-}
-
-func (r rectangle) pArea() {
-	fmt.Println("Rectanngle: ", r.areaIs())
-}
-
-func (s square) pArea() {
-	fmt.Println("Square: ", s.areaIs())
+	printArea(r)
 }
 
 func printArea(sh shape) {
-	sh.pArea()
+	fmt.Println("Area is", sh.calcArea(), "cm^2")
+}
+
+func (t triangle) calcArea() float64 {
+	return 0.5 * t.base * t.height
+}
+func (s square) calcArea() float64 {
+	return s.side * s.side
+}
+func (r rectangle) calcArea() float64 {
+	return r.side1 * r.side2
 }
