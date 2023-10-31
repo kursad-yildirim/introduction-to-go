a088ca50d3f7ae813c2cb28adbba868b5e25c2db
kursad-yildirim <kursad.yildirim@outlook.com>
2023-10-31 16:29:35 +0100

    channels

--- 5.concurrency/concurrency.go
+++ 5.concurrency/concurrency.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	ch := make(chan string)
+	//	ch <- "hi"
+	go func() {
+		ch <- "hi"
+	}()
+	msg := <-ch
+	fmt.Println(msg)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- fmt.Sprintf("message #%v", i)
+		}
+		close(ch)
+	}()
+
+	for msg := range ch {
+		fmt.Println(msg)
+	}
+
+	msg, ok := <-ch
+	fmt.Printf("%#v (ok=%v)", msg, ok)
+
+	// ch <- "hey" // panic, try to recolse will panic, send/receive to a nil channell will block forever why do we have nil channels
+
+	/*
+		for {
+			msg,ok := <-ch
+			if !ok {
+				break
+			}
+			fmt.Println(msg)
+		}
+	*/
 }
